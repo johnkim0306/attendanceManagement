047e78d08f645e1ef218f30210d404a20b4469c8
johnk0306 <mkcokr2176@gmail.com>
2024-03-25 16:53:17 -0400

    Implement middleware to filter requests based on the roles

--- middleware.js
+++ middleware.js
@@ -1,1 +1,24 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    const adminPaths = [
+      "/admin-dashboard",
+    ];
+    const currentPath = req.nextUrl.pathname;
+
+    if (adminPaths.includes(currentPath) && req.nextauth.token?.role !== "ADMIN") {
+      return new NextResponse("You are not authorized!");
+    }
+    
+    return undefined;
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token?.role === "ADMIN",
+    },
+  }
+);
+
+export const config = { matcher: ["/admin-dashboard",] };
