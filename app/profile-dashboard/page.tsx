390b4973ee8d8e4a8d0a38647b0186143fa0d005
johnk0306 <mkcokr2176@gmail.com>
2024-03-28 17:26:15 -0400

    Create a sample profile-dashboard page

--- app/profile-dashboard/page.tsx
+++ app/profile-dashboard/page.tsx
@@ -1,7 +1,26 @@
 'use client';
+import ProfileTable from "@/components/profileTable";
+import ProfileNavbar from "@/components/ProfileNavbar";
+import Link from "next/link";
 
 export default function ProfileDashboard() {
 	return (
-		<div>This is the Admin Dashbaord!</div>
+		<>
+			<div>This is the Admin Dashbaord!</div>
+			<ProfileNavbar />
+
+			<div className="flex justify-center">
+				<Link className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 mr-10" href="/employees">
+					<span className="sr-only">Signed In Employees button</span>
+					All Signed in (17)
+				</Link>
+				<Link className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" href="/employees">
+					<span className="sr-only">Signed In Employees button</span>
+					Employees (20)
+				</Link>
+			</div>
+			<ProfileTable />
+		</>
+		
 	);
 }
